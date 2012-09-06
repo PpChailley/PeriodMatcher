ebcd705530b6252cfec3199b96573f63e107abab
PPC <pierre-paul.chailley@neosynergix.com>
2012-09-06 17:50:24 -0400

    MERGE "Preparing merge with an empty lib as target"

---
+++ PeriodMatcher/Tests/RandomSuppliedValuesTester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gbd.PeriodMatching.Tests
+{
+  public class RandomSuppliedValuesTester : PeriodMatcherTester
+  {
+
+
+
+  }
+}
