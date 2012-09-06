b3301922c49a1283fafbd77df7ea3ef2e25363fc
PPC <pierre-paul.chailley@neosynergix.com>
2012-09-06 17:56:25 -0400

    MERGE "Added prototypes for all methods to be merged"

--- PeriodMatcher/Tests/RandomSuppliedValuesTester.cs
+++ PeriodMatcher/Tests/RandomSuppliedValuesTester.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Gbd.PeriodMatching.Tools;
 using NLog;
 using NUnit.Framework;
 
@@ -21,6 +22,49 @@ namespace Gbd.PeriodMatching.Tests
     {
       throw new NotImplementedException("Just check that this is executed");
     }
+
+    private List<long> GeneratePeriodsForTimer(ICollection<long> timers, int nbPeriods)
+    {
+      throw new NotImplementedException();
+    }
+
+    //[Test]
+    //[Category("SelfTests")]
+    //[Ignore]
+    public long MultiplyByRandomPowerOf2NoOverflow(
+      [Range(0, int.MaxValue, 131)]            int selectedTimerHOB,
+      [Range(0, int.MaxValue, 941)]            int selectedTimerLOB
+      )
+    {
+      //return MultiplyByRandomPowerOf2NoOverflow((uint) selectedTimerHOB, (uint) selectedTimerLOB);
+      throw new NotImplementedException();
+    }
+
+    public long MultiplyByRandomPowerOf2NoOverflow(long selectedTimer)
+    {
+      //SplitLong split = new SplitLong(selectedTimer);
+      //return MultiplyByRandomPowerOf2NoOverflow(split.HOB, split.LOB);
+            throw new NotImplementedException();
+
+    }
+    public long MultiplyByRandomPowerOf2NoOverflow(SplitLong selectedTimer)
+    {
+      //return MultiplyByRandomPowerOf2NoOverflow(selectedTimer.HOB, selectedTimer.LOB);
+            throw new NotImplementedException();
+
+    }
+
+    //[Test]
+    //[Category("SelfTests")]
+    //[Ignore]
+    public long MultiplyByRandomPowerOf2NoOverflow(
+      [Range(0, int.MaxValue, 131)]            uint selectedTimerHOB,
+      [Range(0, int.MaxValue, 941)]            uint selectedTimerLOB
+      )
+    {
+      throw new NotImplementedException();
+    }
+
 
 
   }
