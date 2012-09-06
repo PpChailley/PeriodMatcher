663450247fed0edb280544500414a39a44758004
PPC <pierre-paul.chailley@neosynergix.com>
2012-09-06 17:54:15 -0400

    MERGE "All classes created"
    * Also added a selftest : does NCrunch see this class?

--- PeriodMatcher/Tests/RandomSuppliedValuesTester.cs
+++ PeriodMatcher/Tests/RandomSuppliedValuesTester.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NLog;
+using NUnit.Framework;
 
 namespace Gbd.PeriodMatching.Tests
 {
   public class RandomSuppliedValuesTester : PeriodMatcherTester
   {
 
+
+    private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+    private readonly Random _rnd = new Random(DateTime.Now.Millisecond);
+
+
+    [Test]
+    [Category("SelfTests")]
+    public void NCrunchSmokeTest()
+    {
+      throw new NotImplementedException("Just check that this is executed");
+    }
 
 
   }
---
+++ PeriodMatcher/Tools/SplitLong.cs
@@ -0,0 +1,31 @@
+﻿namespace Gbd.PeriodMatching.Tools
+{
+
+  public class SplitLong
+  {
+    public uint HOB;
+    public uint LOB;
+
+    public SplitLong(long value)
+    {
+      LOB = (uint)(value & uint.MaxValue);
+      HOB = (uint)(value >> 32);
+    }
+
+    public SplitLong(ulong value)
+    {
+      LOB = (uint)(value & uint.MaxValue);
+      HOB = (uint)(value >> 32);
+    }
+
+    public long ToLong()
+    {
+      return (HOB << 32) | LOB;
+    }
+
+    public ulong ToULong()
+    {
+      return (HOB << 32) | LOB;
+    }
+  }
+}
