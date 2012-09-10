44a8d6ed7ee9074c629c88c6f9a8d12c8cd9a53e
PPC <pierre-paul.chailley@neosynergix.com>
2012-09-10 19:36:07 -0400

    REFACTOR "Moved Self-tests to a descendant class"

--- PeriodMatcher/Tests/RandomSuppliedValuesTester.cs
+++ PeriodMatcher/Tests/RandomSuppliedValuesTester.cs
@@ -14,113 +14,7 @@ namespace Gbd.PeriodMatching.Tests
     private readonly Random _rnd = new Random(DateTime.Now.Millisecond);
 
 
-    #region Smoke Self Tests
 
-    [Test]
-    [Category("SelfTests")]
-    [ExpectedException(typeof(NotImplementedException))]
-    public void NCrunchSmokeTest()
-    {
-      throw new NotImplementedException("Just check that this is executed");
-    }
-
-
-    #endregion
-
-    #region Testing helpers
-
-    [Test]
-    [Category("SelfTests")]
-    public void SplitLongTests(
-      [Values(long.MinValue, long.MinValue + 1L, -1L, long.MaxValue - 1L, long.MaxValue)] long selectedTimer)
-    {
-      SplitLong s = new SplitLong(selectedTimer);
-      long rebuilt = s.ToLong();
-
-        Assert.That(rebuilt, Is.EqualTo(selectedTimer));
-    }
-
-    [Test]
-    [Category("SelfTests")]
-    // NUnit is picky with longs that have int-compatible values (looks like it auto-casts them to int32)
-    // For some obscure reason, values 0 and 1 fail the test outside the test code. Better ask StackOverflow someday
-    public void SplitLongTests(
-      [Values(int.MinValue, int.MinValue + 1, -1, int.MaxValue - 1, int.MaxValue)] int selectedTimer)
-    {
-      SplitLong s = new SplitLong(selectedTimer);
-      long rebuilt = s.ToLong();
-
-      Assert.That((int)rebuilt, Is.EqualTo(selectedTimer));
-    }
-
-
-    [Test]
-    [Category("SelfTests")]
-    public void SplitLongTests(
-      [Values(0, 1, ulong.MaxValue-1, ulong.MaxValue)] ulong selectedTimer)
-    {
-      SplitLong s = new SplitLong(selectedTimer);
-      Assert.That(s.ToULong(), Is.EqualTo(selectedTimer));
-    }
-
-
-    #endregion
-
-    #region Self Tests
-
-    [Test]
-    public void AssertIsAPowerOfTwoSelfTestsSuccess(
-      [Range(0, 62, 1)]         int power)
-    {
-      long shouldBeAPowerOf2 = ((long) 1) << power;
-      Assert.That(IsAPowerOfTwo(shouldBeAPowerOf2), Is.True);
-    }
-
-    [Test]
-    [ExpectedException(typeof(AssertionException))]
-    public void AssertIsAPowerOfTwoSelfTestsFailure(
-      [Range(2, 62, 1)]         int power,
-      [Values(-1, 1)]           int offset)
-    {
-      long shouldNotBeAPowerOf2 = ((long)1) << power;
-      shouldNotBeAPowerOf2 += offset;
-      Assert.That(IsAPowerOfTwo(shouldNotBeAPowerOf2), Is.True);
-    }
-
-
-    [Test]
-    public void MultiplyByRandomPowerOf2NoOverflowSelfTests(
-      [Values(0, int.MaxValue, 99999999)]            int HOB,
-      [Values(1, int.MaxValue, 99997598)]            int LOB
-      )
-    {
-        long period = new SplitLong((uint) HOB, (uint) LOB).ToLong();
-        long timer = MultiplyByRandomPowerOf2NoOverflow(HOB, LOB);
-        long multiplier = timer/period;
-
-        Assert.That(multiplier*period, Is.EqualTo(timer), "Generated timer is not a multiple of the period (integer rounding occurred)");
-        Assert.That(IsAPowerOfTwo(multiplier), Is.True);
-    }
-
-
-
-    [Test]
-    public void GeneratePeriodsForTimerTester(
-      [Values(0, 1, 10, 912, MaxS32 - 1, MaxS32)]             int timerHOB,
-      [Values(1, 10, 951, MaxS32-1, MaxS32)]                  int timerLOB,
-      [Values(1, 2, 3, 4, 10, 100, MaxP-1, MaxP)]             int nbPeriods)
-    {
-      SplitLong timer = new SplitLong((uint)timerHOB, (uint)timerLOB);
-
-      var periods = GeneratePeriodsForTimer(timer.ToLong(), nbPeriods);
-
-      Assert.That(periods, Is.All.Matches(new Predicate<long>(IsAPowerOfTwo)));
-    }
-
-
-
-
-    #endregion
 
     #region Helpers
 
@@ -143,10 +37,16 @@ namespace Gbd.PeriodMatching.Tests
 
     #endregion
 
-
-    private List<long> GeneratePeriodsForTimer(long timer, int nbPeriods)
+    protected List<long> GeneratePeriodsForTimer(long timer, int nbPeriods)
     {
-      throw new NotImplementedException();
+      List<long> periods = new List<long>(nbPeriods);
+
+      for (int i = 0; i < nbPeriods; i++)
+      {
+        periods.Add(MultiplyByRandomPowerOf2NoOverflow(timer));
+      }
+
+      return periods;
     }
 
     public long MultiplyByRandomPowerOf2NoOverflow(int selectedTimerHOB,int selectedTimerLOB)
@@ -166,8 +66,8 @@ namespace Gbd.PeriodMatching.Tests
     {
       long selectedTimer = (((long) selectedTimerHOB) << 32) | selectedTimerLOB;
 
-      Log.Warn("");
-      Log.Warn("Trying to generate a multiplier for timer value 0x {0:X8} {0:X8} = {0:X16}", selectedTimerHOB, selectedTimerLOB, selectedTimer);
+      //Log.Warn("");
+      //Log.Warn("Trying to generate a multiplier for timer value 0x {0:X8} {0:X8} = {0:X16}", selectedTimerHOB, selectedTimerLOB, selectedTimer);
 
       long maxMultiplierForNotOverflowing = (long.MaxValue/selectedTimer);
       double maxLog2ForNotOverflowing = Math.Log(maxMultiplierForNotOverflowing, 2);
